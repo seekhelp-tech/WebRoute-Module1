9043b6ead8d603aa8e3b9c75995a104353e3ff87
HackerCTF <76810151+HackerCTF@users.noreply.github.com>
2020-12-31 15:31:54 +0530

    Create webroute.py

---
+++ webroute.py
@@ -0,0 +1,14 @@
+import requests
+import os
+a = open("wordlist.txt", "r")
+b = a.readlines()
+c = len(b)
+d = open("wordlist.txt", "r")
+for i in range(c):
+    e = d.readline()
+    f = e[0:len(e)-1]
+    print(f)
+    g = requests.get("https://uditsingh.com/"+f)
+    print("https://uditsingh.com/"+f)
+    print("\n", g.status_code)
+    g.text
